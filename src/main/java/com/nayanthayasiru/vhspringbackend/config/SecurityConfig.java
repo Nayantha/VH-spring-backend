1abdcaa92e4a0b6fde2763aeddfff582831770b2
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-04-05 15:56:03 +0530

    add the bean wrapper to security filter chain.

--- src/main/java/com/nayanthayasiru/vhspringbackend/config/SecurityConfig.java
+++ src/main/java/com/nayanthayasiru/vhspringbackend/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.nayanthayasiru.vhspringbackend.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,6 +12,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.httpBasic(withDefaults());
         return http.build();
