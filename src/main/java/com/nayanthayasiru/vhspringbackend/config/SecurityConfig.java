729ed7428fdc0cd69d925066db395bcfb7a4d595
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-04-05 15:21:44 +0530

    add security config file.

---
+++ src/main/java/com/nayanthayasiru/vhspringbackend/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.nayanthayasiru.vhspringbackend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+}
