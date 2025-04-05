c547398cba9cb341e701418417ef4650dce2c49c
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-04-05 06:49:11 +0530

    Initial Commit.

---
+++ src/main/java/com/nayanthayasiru/vhspringbackend/VHSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nayanthayasiru.vhspringbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VHSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VHSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nayanthayasiru/vhspringbackend/VHSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nayanthayasiru.vhspringbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VHSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
