d31b689dd36c89f50b18b57bfa380fa096cdc776
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-04-13 17:02:25 +0530

    add a new method to create customer object from DTO.

--- src/main/java/com/nayanthayasiru/vhspringbackend/models/DTOs/RegisterRequestDTO.java
+++ src/main/java/com/nayanthayasiru/vhspringbackend/models/DTOs/RegisterRequestDTO.java
@@ -1,7 +1,9 @@
 package com.nayanthayasiru.vhspringbackend.models.DTOs;
 
+import com.nayanthayasiru.vhspringbackend.models.Customer;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public record RegisterRequestDTO(
         @NotBlank String password,
@@ -9,4 +11,14 @@ public record RegisterRequestDTO(
         @NotBlank String firstName,
         @NotBlank String lastName,
         @NotBlank @Email String email
-) {}
+) {
+    public static Customer fromDTO(RegisterRequestDTO dto, PasswordEncoder passwordEncoder) {
+        Customer customer = new Customer();
+        customer.setUsername(dto.username());
+        customer.setPassword(passwordEncoder.encode(dto.password()));
+        customer.setFirstName(dto.firstName());
+        customer.setLastName(dto.lastName());
+        customer.setEmail(dto.email());
+        return customer;
+    }
+}
