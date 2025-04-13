d1a8705563944a3933723030a27275e1214331ef
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-04-13 17:08:16 +0530

    implement the methods in the interface.

--- src/main/java/com/nayanthayasiru/vhspringbackend/services/CustomerDetailsService.java
+++ src/main/java/com/nayanthayasiru/vhspringbackend/services/CustomerDetailsService.java
@@ -1,20 +1,39 @@
 package com.nayanthayasiru.vhspringbackend.services;
 
 import com.nayanthayasiru.vhspringbackend.models.Customer;
+import com.nayanthayasiru.vhspringbackend.models.DTOs.AuthResponseDTO;
+import com.nayanthayasiru.vhspringbackend.models.DTOs.RegisterRequestDTO;
 import com.nayanthayasiru.vhspringbackend.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomerDetailsService implements UserDetailsService {
+public class CustomerDetailsService implements UserDetailsService, CustomerDetailsServiceInterface {
     @Autowired
     private CustomerRepository customerRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return customerRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User name not found."));
+    }
+
+    @Override
+    public AuthResponseDTO registerCustomer(RegisterRequestDTO registerRequestDTO) {
+        Customer newCustomer = RegisterRequestDTO.fromDTO(registerRequestDTO, passwordEncoder);
+
+        Customer savedCustomer = customerRepository.save(newCustomer);
+
+        return AuthResponseDTO.fromClass(savedCustomer);
+    }
+
+    @Override
+    public boolean existsByUsername(String username) {
+        return customerRepository.existsByUsername(username);
     }
 }
