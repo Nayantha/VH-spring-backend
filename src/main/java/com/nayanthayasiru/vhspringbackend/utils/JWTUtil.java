155de2b84741ae27def6289e6fadd1fa22c29868
Nayantha Yasiru <88623037+Nayantha@users.noreply.github.com>
2025-05-02 14:33:43 +0530

    create jwt token generator.

---
+++ src/main/java/com/nayanthayasiru/vhspringbackend/utils/JWTUtil.java
@@ -0,0 +1,31 @@
+package com.nayanthayasiru.vhspringbackend.utils;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    @Value("${jwt.expiration}")
+    private long EXPIRATION_TIME;
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    private SecretKey getSigningKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+}
